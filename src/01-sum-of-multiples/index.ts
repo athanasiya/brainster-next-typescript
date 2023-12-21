fcf55360488e44c1faa0b7ad085378d0ae671602
Atanasija Gjorgievski <gjorgievskiatanasija@gmail.com>
2023-12-21 22:46:09 +0100

    Completed Lab

--- src/01-sum-of-multiples/index.ts
+++ src/01-sum-of-multiples/index.ts
@@ -1,1 +1,12 @@
-export function sumOfMultiples(multiple, max) {}
+export function sumOfMultiples(multiple: number, max: number): number {
+  if (multiple > max) {
+    return 0
+  }
+  let sum: number = 0
+  for (let i = 0; i <= max; i++) {
+    if (i % multiple == 0) {
+      sum += i
+    }
+  }
+  return sum
+}
